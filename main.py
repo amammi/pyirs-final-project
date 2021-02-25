2bca18cd829fdde4c9aa653b2c29cce89c7c3076
amammi <andrea.mammi@windtre.it>
2021-02-25 10:57:54 +0100

    reformat e commenti

--- main.py
+++ main.py
@@ -5,33 +5,8 @@ import speech_recognition
 
 from utils import Commons, VocalSentencies
 from vocals.service import VocalService
-from weather.service import WeatherService
+# from weather.service import WeatherService
 from wiki.service import WikipediaService
-
-
-def dirtyVersion():
-    tts = gtts.gTTS("Ciao, cosa posso fare per te?", lang="it")
-    tts.save("prova.wav")
-    audioplayer.AudioPlayer("prova.wav").play(block=True)
-    r = speech_recognition.Recognizer()
-    with speech_recognition.Microphone() as source:
-        r.adjust_for_ambient_noise(source, duration=1)
-        print("Inizio registrazione...")
-        audio = r.record(source, duration=5)
-        print("Registrazione terminata.")
-    request = r.recognize_google(audio, language="it-IT")
-    if "tempo" in request:
-        tts = gtts.gTTS("Il tempo sarà buono, domani", lang="it")
-        tts.save("prova.wav")
-        audioplayer.AudioPlayer("prova.wav").play(block=True)
-        time.sleep(1)
-    else:
-        tts = gtts.gTTS("Mi dispiace, non ho capito", lang="it")
-        tts.save("prova.wav")
-        audioplayer.AudioPlayer("prova.wav").play(block=True)
-    tts = gtts.gTTS("Ciao ciao!", lang="it")
-    tts.save("prova.wav")
-    audioplayer.AudioPlayer("prova.wav").play(block=True)
 
 
 def main():
@@ -50,14 +25,14 @@ def main():
                     vocalService.saySomething(VocalSentencies.VOCAL_ERROR_NOT_FOUND.value)
             else:
                 vocalService.saySomething(VocalSentencies.VOCAL_ERROR_MESSAGE.value)
-        elif vocalService.isWeatherRequest(request):
-            city = vocalService.findWeatherCityInRequest(request)
-            if city is None:
-                vocalService.saySomething(VocalSentencies.VOCAL_CITY_NOT_FOUND.value)
-                return
-            temperature = weatherService.getWeatherCity(city)
-            weatherPhrase = weatherService.getWeatherPhrase(city, temperature)
-            vocalService.saySomething(weatherPhrase)
+        # elif vocalService.isWeatherRequest(request):
+        #     city = vocalService.findWeatherCityInRequest(request)
+        #     if city is None:
+        #         vocalService.saySomething(VocalSentencies.VOCAL_CITY_NOT_FOUND.value)
+        #         return
+        #     temperature = weatherService.getWeatherCity(city)
+        #     weatherPhrase = weatherService.getWeatherPhrase(city, temperature)
+        #     vocalService.saySomething(weatherPhrase)
         else:
             vocalService.saySomething(VocalSentencies.VOCAL_ERROR_MESSAGE.value)
         vocalService.saySomething(VocalSentencies.VOCAL_ANOTHER_REQUEST.value)
@@ -66,7 +41,7 @@ def main():
     vocalService.saySomething(VocalSentencies.VOCAL_BYE_BYE.value)
 
 
-weatherService = WeatherService()
+# weatherService = WeatherService()
 wikiService = WikipediaService()
 vocalService = VocalService()
 
--- vocals/service.py
+++ vocals/service.py
@@ -4,19 +4,31 @@ import speech_recognition as sr
 
 class VocalService(object):
     def __init__(self):
+        # ---- Configurazione del modulo pyttsx3 ----
         self.engine = pyttsx3.init(driverName="nsss")
         self.engine.setProperty("volume", 1.0)
         self.engine.setProperty('rate', 150)
 
+        # ---- Configurazione del modulo SpeechRecognition ----
         self.microphone = sr.Microphone()
         self.recognizer = sr.Recognizer()
         self.lang = "it_IT"
 
     def saySomething(self, phrase: str):
+        """
+        Metodo per far parlare il computer
+        :param phrase: la frase da far dire al computer
+        :return: None
+        """
         self.engine.say(phrase)
         self.engine.runAndWait()
 
     def listenSomething(self, duration: int = 5) -> str:
+        """
+        Metodo per far ascoltare il pc e trascrivere tutto in una stringa
+        :param duration: durata (in secondi) di ascolto
+        :return: la stringa trascritta oppure None
+        """
         with self.microphone as source:
             self.recognizer.adjust_for_ambient_noise(source)
             print("Inizio registrazione...")
@@ -25,10 +37,20 @@ class VocalService(object):
             return self.recognizer.recognize_google(record, language=f"{self.lang}")
 
     def isWikipediaRequest(self, sentence: str):
+        """
+        Metodo utile per capire se dalla frase detta, vogliamo cercare o meno su Wikipedia
+        :param sentence: la frase trascritta
+        :return: True se Wikipedia è all'interno della frase detta oppure False
+        """
         return "wikipedia" in sentence or "Wikipedia" in sentence
 
-    def findWikiSentenceInRequest(self, request: str) -> str:
-        tokens = request.split(" ")
+    def findWikiSentenceInRequest(self, sentence: str) -> str:
+        """
+        Metodo per trovare l'oggetto della nostra richiesta su Wikipedia
+        :param sentence: la frase pronunciata dall'utente e trascritta
+        :return: la parte della stringa contenente la ricerca da effettuare
+        """
+        tokens = sentence.lower().split(" ")
         try:
             index = tokens.index("wikipedia")
             return " ".join(tokens[index + 1:])
@@ -36,6 +58,11 @@ class VocalService(object):
             return None
 
     def isPositiveSentence(self, sentence: str) -> bool:
+        """
+        Metodo utile per capire se l'utente ha detto si oppure no
+        :param sentence: la frase pronunciata
+        :return: True se la frase contiente "sì", altrimenti False
+        """
         return "si" in sentence.lower() or "sì" in sentence.lower()
 
     def isWeatherRequest(self, request: str):
